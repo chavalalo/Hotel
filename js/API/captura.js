b3f73e72063631db8ce584d7e93c4b09732417c5
Carlos Celma <kaayro@gmail.com>
2013-05-03 20:31:00 -0500

    ASa

--- js/API/captura.js
+++ js/API/captura.js
@@ -2,7 +2,7 @@
 function tomarFoto(){
 	navigator.device.capture.captureImage(function(mediaFiles){
 		path = mediaFiles[0].fullPath;
-		$('#regFoto').append('<img src="'+path+'" width="100%" />').attr('rel',path);
+		$('#regFoto').append('<br><img src="'+path+'" width="100%" />').attr('rel',path);
 	},function(error){
 		navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
 	},{limit:2});
