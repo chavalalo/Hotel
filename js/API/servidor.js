89399c747e7e625842441789fb35b9ee2b7d8dcc
Carlos Celma <kaayro@gmail.com>
2013-05-03 20:03:47 -0500

    fhgf

--- js/API/servidor.js
+++ js/API/servidor.js
@@ -1,21 +1,13 @@
 //Servidor
-function enviarRegistro(nombre,telefono,email){
+function enviarRegistro(nombre,telefono,email,foto){
 	$.ajax({
 		type: "POST",
 		url: "http://igitsoft.com/pgtest.php",
 		data: "nom="+nombre+"&tel="+telefono+"&ema="+email+"&id="+disp()['id']
 	}).done(function(msg){
 		if(msg==1){
-			navigator.notification.confirm("Datos Guardos Satisfactoriamente\n"+disp()['platform'], function(botones){
-				switch(botones){
-					case 1:
-						navigator.notification.beep(5);
-						break;
-					case 2:
-						navigator.notification.vibrate(500);
-						break;	
-				}
-			}, "Titulo", "Beep, Vibrar, Salir");
+			//Subir Foto
+			uploadPhoto(foto);
 		}else{
 			navigator.notification.alert("Los datos no fueron enviados correctamente", null, "Error de Registro", "Aceptar");
 		}
