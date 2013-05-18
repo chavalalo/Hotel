c26ea205fa1def5c4c5f9f4b03185a460f265088
Salvador Razo <salvador.razo@nuvemtecnologia.mx>
2013-05-17 19:36:20 -0500

    Almacenamiento

---
+++ js/API/almacenamiento.js
@@ -0,0 +1,13 @@
+// Almacenamiento
+function saveLogin(name,id){
+	window.localStorage.setItem('nombre',name);
+	window.localStorage.setItem('id',id);
+}
+
+function isLogin(){
+	var id = window.localStorage.getItem('id');		
+	if(id != undefined)
+		return true;
+	else
+		return false;
+}
