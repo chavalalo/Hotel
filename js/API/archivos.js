5b2ab98a34724c51367f9a61c1220e75f26650ae
Salvador Razo <salvador.razo@nuvemtecnologia.mx>
2013-05-17 19:53:30 -0500

    cambio en login

--- js/API/archivos.js
+++ js/API/archivos.js
@@ -18,10 +18,15 @@ function uploadPhoto(foto, nom) {
 			switch(botones){
 				case 1:
 					navigator.notification.beep(5);
+					window.location.href = '#page';
 					break;
 				case 2:
 					navigator.notification.vibrate(500);
+					window.location.href = '#page';
 					break;	
+				case 3:
+					window.location.href = '#page';
+					break;
 			}
 		}, "Titulo", "Beep, Vibrar, Salir");
 	}, function(err){
