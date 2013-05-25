c3d49e686617e26eb738bc271bf89909b1d3fdfb
Salvador Razo <salvador.razo@nuvemtecnologia.mx>
2013-05-24 19:54:47 -0500

    reservas

--- js/API/servidor.js
+++ js/API/servidor.js
@@ -13,3 +13,21 @@ function enviarRegistro(nombre,telefono,email,foto){
 		}
 	});
 }
+
+function reservarHb(t,p,h,d) {
+		$.ajax({
+		type: "POST",
+		url: "http://igitsoft.com/pgtest.php",
+		data: "t="+t+"&p="+p+"&h="+h+"&d="+d
+	}).done(function(msg){
+		if(msg==1){
+			navigator.notification.alert("Reserva realizada correctamente", function() {
+				window.location.href = '#page';
+				}, "Reserva","Aceptar");
+		}else{
+			navigator.notification.alert("No se pudo sincronizar la reserva, Esperando conexión a internet", null, "Error de Sincronización", "Aceptar");
+		}
+	});
+	
+	
+	}
